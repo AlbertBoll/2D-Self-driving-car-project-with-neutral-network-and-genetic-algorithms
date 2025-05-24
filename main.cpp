3b0ecda7d4040fbaa3e0beee84b0888fb4f39c7d
Boer <albertboll19900124@gmail.com>
2025-05-24 11:36:20 -0700

    clean the main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,12 @@
 
 int main(int, char**)
 {
-    
     WindowProperties window_properties;
     window_properties.m_Title = "Genetic Algorithm Simulator";
     window_properties.m_Width = 1280;
     window_properties.m_Height = 720;
     window_properties.flag = WindowFlags::NONE;
     auto& genetic_algorithm_simulator = GeneticAlgorithmSimulator::GetInstance().SetTotalSimulationRound(50);
-    //ScopedPtr<Simulator> genetic_algorithm_simulator = CreateScopedPtr<GeneticAlgorithmSimulator>();
     genetic_algorithm_simulator.Initialize(window_properties);
     genetic_algorithm_simulator.Run();
-
-
 }
